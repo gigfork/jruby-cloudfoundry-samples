c4696dabea33bbc8f074bbf6e4eedd16a0fff667
Satoru Chinen <satoru.chinen@gmail.com>
2011-12-11 21:41:59 +0900

    Add Rails 3.0.x(3.0.5) Redis application sample.

---
+++ rails3/redis_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RedisApp::Application.config.secret_token = 'e19aa6de6fe7278cbc86268a0f323a6c0f017461e7692e3cb818f7d5eba16fba3c51f3098ffa5f3fd54fef52316ac62748e7a98643a441ca85ac1c12d3ef66dd'
